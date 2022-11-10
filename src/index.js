1b346c85652411d0eca75fa75e86ee7bf28a0584
santiago <santiagodeandrade4@gmail.com>
2022-11-10 07:52:37 -0400

    Commit de imgs de miamorcito ;(

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import { addMouseAnimation } from './addMouseAnimation';
 import { updateParticles } from './mouseAnimationDependencies/updateParticles';
 import { renderParticles} from "./mouseAnimationDependencies/renderParticles"
 import "./particles.css"
+import { createRoot } from 'react-dom/client';
 
 let particlesList = []
+let root = createRoot(document.getElementById('root'))
 export const particlesClassName = "particle"
 export let counter = 0;
 
-addMouseAnimation(particlesList)
+root.render(
 
+)
+
+addMouseAnimation(particlesList)
 setInterval(() => {
   updateParticles(particlesList)
   renderParticles(particlesList)
