049c7ab48ced4de3ea6d2db1e22757b0f5229658
santiago <santiagodeandrade4@gmail.com>
2022-11-15 22:07:38 -0400

    actualizacion de responsive

--- src/particlesAnimationDependencies/setParticlesInterval.js
+++ src/particlesAnimationDependencies/setParticlesInterval.js
@@ -2,7 +2,8 @@ import { updateParticles } from './updateParticles';
 import { renderParticles} from "./renderParticles"
 
 export function setParticlesInterval({particlesList}){
-    let updateTimer = window.innerWidth > 500? 10 : 50;
+    let updateTimer = window.innerWidth > 500? 10 : 30;
+    console.log(updateTimer)
     setInterval(() => {
         if (particlesList.length !== 0){
             updateParticles(particlesList)
