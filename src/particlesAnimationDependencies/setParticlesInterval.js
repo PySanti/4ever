72e0ae63b80e1a926a909e2d364829b1722bc943
santiago <santiagodeandrade4@gmail.com>
2022-11-15 22:00:29 -0400

    actualizacion de initial title y timer de update de particulas para responsive

--- src/particlesAnimationDependencies/setParticlesInterval.js
+++ src/particlesAnimationDependencies/setParticlesInterval.js
@@ -2,10 +2,11 @@ import { updateParticles } from './updateParticles';
 import { renderParticles} from "./renderParticles"
 
 export function setParticlesInterval({particlesList}){
+    let updateTimer = window.innerWidth > 500? 10 : 50;
     setInterval(() => {
         if (particlesList.length !== 0){
             updateParticles(particlesList)
             renderParticles(particlesList)
         }
-    }, 10)
+    }, updateTimer)
 }
