6997f490bacff907334dc9753f0a2774527d92a2
santiago <santiagodeandrade4@gmail.com>
2022-11-16 22:52:05 -0400

    .

--- src/lateralMsgDependencies/generatePos.js
+++ src/lateralMsgDependencies/generatePos.js
@@ -8,7 +8,7 @@ export function generatePos({lateralMsg = false, lateralMsgSize = null}){
         pos[0] = -lateralMsgSize[0];
         pos[1] = generateRandomNumber(titleHeight + (window.innerHeight*0,3), window.innerHeight - lateralMsgSize[1])
     } else{
-        let generationRangeX  = [50, 80] 
+        let generationRangeX  = [60, 80] 
         let generationRangeY = [30, 70] 
         pos[0] = generateRandomNumber((window.innerWidth*generationRangeX[0]/100), (window.innerWidth*generationRangeX[1]/100))
         pos[1] = generateRandomNumber((window.innerHeight*generationRangeY[0]/100), (window.innerHeight*generationRangeY[1]/100))
